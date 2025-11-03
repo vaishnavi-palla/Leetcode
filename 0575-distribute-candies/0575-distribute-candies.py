4b58fb4620bedd71bbb8f1ace3f80a2359132c8c
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-11-03 20:50:55 +0530

    Time: 20 ms (60.9%), Space: 19.7 MB (25.42%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,8 +1,3 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        vari = set(candyType)
-        can_eat = len(candyType)//2
-        if len(vari) < can_eat:
-            return len(vari)
-        else:
-            return can_eat
+        return min(len(set(candyType)),len(candyType)//2)
