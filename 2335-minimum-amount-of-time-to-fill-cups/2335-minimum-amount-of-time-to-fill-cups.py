8224e91d1518f2bf045f051eb084c5d02cedfb47
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-11-13 21:35:13 +0530

    Time: 0 ms (100%), Space: 17.6 MB (97.72%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -1,13 +1,3 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
-            if amount[0] == 0 and amount[1] == 0 and amount[2] == 0:
-                return 0
-            elif amount[0] == 0 and amount[1] == 0 and amount[2] > 0:
-                return amount[2]
-            elif amount[0] == 0 and amount[1] > 0 and amount[2] == 0:
-                return amount[1]
-            elif amount[0] > 0 and amount[1] == 0 and amount[2] == 0:
-                return amount[0]
-            else:
-                summ = sum(amount)
-                return (summ%2) + (summ//2)
+        return max(max(amount),(sum(amount)+1)//2)
