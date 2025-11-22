3570891d99c39e2d24b12bf0db7a7c2400c063ee
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-11-22 21:00:56 +0530

    Time: 7 ms (47.21%), Space: 18 MB (23.75%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -1,23 +1,15 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        dum = []
-        for s in emails:
-            start = s.find('+')
-            end = s.find('@')
-            if start != -1 and end != -1 and start < end:
-                dum.append(s[:start] + s[end:])
-            else:
-                dum.append(s)
-        res = set()
-        for mail in dum:
-            em = ''
-            for i in range (len(mail)):
-                if mail[i] == '@':
-                    em += mail[i:]
-                    break
-                if mail[i] == '.':
-                    pass
-                else:
-                    em += mail[i]
-            res.add(em)
-        return len(res)
+        uniq = set()
+
+        for email in emails:
+            local, domain = email.split("@")
+
+            if "+" in local:
+                local = local.split("+")[0]
+
+            name = local.replace(".", "")
+
+            uniq.add(f"{name}@{domain}")
+
+        return len(uniq)
