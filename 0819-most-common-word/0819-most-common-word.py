d3d8e3321091be5638aa0004d5456d9bfabe5abc
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-11-27 20:44:24 +0530

    Time: 0 ms (100%), Space: 17.9 MB (28.93%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,13 +1,7 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        para = []
-        for word in paragraph.replace('.',' ').replace(',',' ').split():
-            dum = ''
-            for i in word:
-                if i.isalpha():
-                    dum = dum+i
-            para.append(dum.lower())
-        dum = [w for w in para if w not in banned]
+        para = re.split(r'[\!\?\'\,;:.\s]+', paragraph.lower())
+        dum = [w for w in para if w and w not in banned]
         counts = Counter(dum)
         maxi = max(counts,key=counts.get)
         return maxi
