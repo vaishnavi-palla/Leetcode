b8a9cdb9a422ee34698bdfa6f79e75217fe6cb62
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-12-01 21:34:44 +0530

    Time: 17 ms (5.05%), Space: 17.7 MB (68.9%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            seen = set()
+            for i in row:
+                if i in seen and i != '.':
+                    return False
+                seen.add(i)
+        for i in range(len(board)):
+            seen = set()
+            for j in range(len(board)):
+                if board[j][i] in seen and board[j][i] != '.':
+                    return False
+                seen.add(board[j][i])
+
+            for row in range(0,9,3):
+                for col in range(0,9,3):
+                    seen = set()
+                    for i in range(3):
+                        for j in range(3):
+                            cell = board[row + i][col + j]
+                            if cell in seen and cell != '.':
+                                return False
+                            seen.add(cell)
+        return True
