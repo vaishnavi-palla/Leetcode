ecf97bb4c14eb9eaf1e6dc763ab126ca88d32104
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-12-02 19:21:44 +0530

    Time: 21 ms (76.38%), Space: 18.7 MB (23.12%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,25 +1,13 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
-        lst = [i+1 for i in range (n)]
-        counts = Counter(i for row in matrix for i in row)
-        for num,freq in counts.items():
-            if freq>n:
-                return False
-
         for row in matrix:
-            if len(set(row)) != len(lst):
+            if len(set(row)) != n:
                 return False
-            for i in row:
-                if i not in lst:
-                    return False
-
         for i in range(n):
             seen = set()
             for j in range(n):
-                if matrix[j][i] not in lst:
-                    return False
                 seen.add(matrix[j][i])
-            if len(seen) != len(lst):
+            if len(seen) != n:
                 return False
         return True
