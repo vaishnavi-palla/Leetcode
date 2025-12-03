1fc92156d89836c2287a77774a4f76ce9a5e935a
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-12-03 21:05:26 +0530

    Time: 1197 ms (64.3%), Space: 17.9 MB (82.12%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -3,9 +3,6 @@ class Solution:
         while len(nums) > 1:
             newnums = [0]*(len(nums)-1)
             for i in range(len(nums)-1):
-                if nums[i]+nums[i+1] < 10:
-                    newnums[i] = nums[i]+nums[i+1]
-                else:
-                    newnums[i] = (nums[i]+nums[i+1])%10
+                newnums[i] = (nums[i]+nums[i+1])%10
             nums = newnums
         return nums[0]
