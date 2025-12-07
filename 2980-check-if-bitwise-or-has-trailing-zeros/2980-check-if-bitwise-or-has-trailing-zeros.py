9dd74f37c6d625c4a9b6f555e84ed99b28507cd0
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-12-07 21:42:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (49.78%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.py
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.py
@@ -4,4 +4,6 @@ class Solution:
         for i in nums:
             if i%2==0:
                 even+=1
-        return even>=2
+                if even >= 2:
+                    return True
+        return False
