485ee0bf4c5203384341616152919697b73065c4
vaishnavi-palla <23p31a4255@acet.ac.in>
2025-12-23 22:50:08 +0530

    Time: 20 ms (5.47%), Space: 17.2 MB (98.42%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,23 +3,23 @@ class Solution:
         for row in board:
             seen = set()
             for i in row:
-                if i in seen and i != '.':
+                if i in seen and i != ".":
                     return False
                 seen.add(i)
         for i in range(len(board)):
             seen = set()
             for j in range(len(board)):
-                if board[j][i] in seen and board[j][i] != '.':
+                if board[j][i] in seen and board[j][i] != ".":
                     return False
                 seen.add(board[j][i])
 
-            for row in range(0,9,3):
-                for col in range(0,9,3):
+            for row in range(0, 9, 3):
+                for col in range(0, 9, 3):
                     seen = set()
                     for i in range(3):
                         for j in range(3):
                             cell = board[row + i][col + j]
-                            if cell in seen and cell != '.':
+                            if cell in seen and cell != ".":
                                 return False
                             seen.add(cell)
         return True
