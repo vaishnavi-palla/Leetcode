1f7561e92424b0469f267022569fe6ada91cf98a
vaishnavi-palla <23p31a4255@acet.ac.in>
2026-01-01 14:32:41 +0530

    Time: 24 ms (5.09%), Space: 18.3 MB (77.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,14 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        m,n = len(matrix),len(matrix[0])
         row = []
         col = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == 0:
                     row.append(i)
                     col.append(j)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        for i in range(m):
+            for j in range(n):
                 if i in row or j in col:
                     matrix[i][j] = 0
