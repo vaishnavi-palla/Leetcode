8bb09472f8b86481dfd2a373d08e6caa379cf67f
vaishnavi-palla <23p31a4255@acet.ac.in>
2026-01-05 23:22:37 +0530

    Time: 5 ms (78.84%), Space: 17.3 MB (93.78%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.py
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.py
@@ -2,48 +2,33 @@ class Solution:
     def countValidWords(self, sentence: str) -> int:
         res = 0
         for word in sentence.split():
-            if word.isalpha():
-                res+=1
-            
-            else:
-                req = False
-                h,c,f,e=0,0,0,0
-                for i in range(len(word)):
-                    if word[i] == '-':
-                        if i == len(word)-1:
-                            req = False
-                            break
-                        else:
-                            h += 1
-                            if word[i-1].isalpha() and word[i+1].isalpha() and h<=1 and i != (len(word))-1:
-                                req = True
-                            else:
-                                req = False
-                                break
-                    if word[i] == ',':
-                        c += 1
-                        if i == len(word)-1 and c <= 1:
-                            req = True
-                        else:
-                            req = False
-                            break
-                    if word[i] == '.' :
-                        f += 1
-                        if i == len(word)-1 and f <= 1:
-                            req = True
-                        else:
-                            req = False
-                            break
-                    if word[i] == '!' :
-                        e += 1
-                        if i == len(word)-1 and e <= 1:
-                            req = True
-                        else:
-                            req = False
-                            break
-                    if word[i] in '0987654321':
-                        req = False
+            hyphen = 0
+            punct = 0
+            valid = True
+
+            for i,ch in enumerate(word):
+                if ch.isdigit():
+                    valid = False
+                    break
+                if ch in '!.,':
+                    if i != len(word)-1:
+                        valid = False
                         break
-                if req == True:
-                    res+=1
+                    punct += 1
+                    if punct > 1:
+                        valid = False
+                        break
+                elif ch == '-':
+                    if i == 0 or i == len(word)-1 or not word[i-1].isalpha() or not word[i+1].isalpha():
+                        valid = False
+                        break
+                    hyphen += 1
+                    if hyphen > 1:
+                        valid = False
+                        break
+                elif not ch.isalpha():
+                    valid = False
+                    break
+            if valid:
+                res += 1
         return res
