c43bfdc493100345a21de0b386dca51b82046934
vaishnavi-palla <23p31a4255@acet.ac.in>
2026-01-07 09:42:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (14.69%) - LeetHub

---
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countElements(self, nums: List[int]) -> int:
+        nums.sort()
+        small = min(nums)
+        big = max(nums)
+        res = []
+        for i in nums:
+            if i != small and i != big:
+                res.append(i)
+        return len(res)
